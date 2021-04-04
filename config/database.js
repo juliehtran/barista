5a52f2e406ca537f89fdca5ea6111c112e561cc7
Julie Tran <tranjhdesign@gmail.com>
2021-04-03 23:15:39 -0400

    Completed barista

--- config/database.js
+++ config/database.js
@@ -1,9 +1,5 @@
 // config/database.js
 module.exports = {
-    'url' : "mongodb+srv://dashlin:sermeil15@dashbase.olf7t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    'url' : process.env.DATABASE_URL,
     'dbName': 'database'
 };
-
-/*
-heroku config:set MONGODB_URI="mongodb+srv://wadeMongo:wade2021@cluster0.uusdg.mongodb.net/moodTracker?retryWrites=true&w=majority"
-*/
